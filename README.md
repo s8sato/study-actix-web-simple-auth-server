71e7cacaed873efc8a24e4fac34ee43e870edc93
satu-n <49983831+satu-n@users.noreply.github.com>
2020-10-02 18:01:51 +0900

    SETUP THE BASE APP

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 ## PREREQUISITE
 
-ひとまず `diesel` `postgresql` を無視して先へ
+ひとまず `postgresql` `diesel_cli` を無視して先へ
 
 ## LET’S BEGIN
 
@@ -17,13 +17,55 @@ $ rustc --version && cargo --version
 rustc 1.46.0 (04488afe3 2020-08-24)
 cargo 1.46.0 (149022b1d 2020-07-17)
 $ echo "1.46.0" > rust-toolchain
-$ sudo apt install libssl-dev pkg-config
+<!-- $ sudo apt install libssl-dev pkg-config -->
 ```
 この後色々やるがどうも `argonautica` がらみで build できない  
 `rust-argon2` に代えて、ひとまず build に成功
 
 ## SETUP THE BASE APP
 
+ここで `postgresql` `diesel_cli` が必要になるので用意
+
+```
+$ sudo apt-get install postgresql-12
+<!-- $ sudo apt install libpq-dev -->
+$ cargo install diesel_cli --no-default-features --features postgres
+```
+
+データベース接続情報を用意
+
+```
+$ sudo passwd postgres
+$ sudo -u postgres createdb simple_auth
+$ echo DATABASE_URL=postgres://postgres:password@localhost/simple_auth > .env
+```
+> At this stage your server should compile
+
+とあるが、まだコンパイルは通らないはずです
+
+```
+error[E0583]: file not found for module `schema`
+  --> src/main.rs:10:1
+   |
+10 | mod schema;
+   | ^^^^^^^^^^^
+   |
+   = help: to create the module `schema`, create file "src/schema.rs"
+
+error[E0433]: failed to resolve: use of undeclared type or module `utils`
+  --> src/main.rs:36:43
+   |
+36 |                 CookieIdentityPolicy::new(utils::SECRET_KEY.as_bytes())
+   |                                           ^^^^^ use of undeclared type or module `utils`
+
+error[E0433]: failed to resolve: use of undeclared type or module `invitation_handler`
+  --> src/main.rs:50:51
+   |
+50 | ...                   .route(web::post().to(invitation_handler::post_invitation)),
+   |                                             ^^^^^^^^^^^^^^^^^^ use of undeclared type or module `invitation_handler`
+```
+
+## SETTING UP DIESEL AND CREATING OUR USER MODEL
 
 
 [tutorial]: https://gill.net.in/posts/auth-microservice-rust-actix-web1.0-diesel-complete-tutorial/
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,67 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate diesel;
+
+use actix_identity::{CookieIdentityPolicy, IdentityService};
+use actix_web::{middleware, web, App, HttpServer};
+use diesel::prelude::*;
+use diesel::r2d2::{self, ConnectionManager};
+
+mod models;
+mod schema;
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    dotenv::dotenv().ok();
+    std::env::set_var(
+        "RUST_LOG",
+        "simple-auth-server=debug,actix_web=info,actix_server=info",
+    );
+    env_logger::init();
+    let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+
+    // create db connection pool
+    let manager = ConnectionManager::<PgConnection>::new(database_url);
+    let pool: models::Pool = r2d2::Pool::builder()
+        .build(manager)
+        .expect("Failed to create pool.");
+    let domain: String = std::env::var("DOMAIN").unwrap_or_else(|_| "localhost".to_string());
+
+    // Start http server
+    HttpServer::new(move || {
+        App::new()
+            .data(pool.clone())
+            // enable logger
+            .wrap(middleware::Logger::default())
+            .wrap(IdentityService::new(
+                CookieIdentityPolicy::new(utils::SECRET_KEY.as_bytes())
+                    .name("auth")
+                    .path("/")
+                    .domain(domain.as_str())
+                    .max_age_time(chrono::Duration::days(1))
+                    .secure(false), // this can only be true if you have https
+            ))
+            // limit the maximum amount of data that server will accept
+            .data(web::JsonConfig::default().limit(4096))
+            // everything under '/api/' route
+            .service(
+                web::scope("/api")
+                    .service(
+                        web::resource("/invitation")
+                            .route(web::post().to(invitation_handler::post_invitation)),
+                    )
+                    // .service(
+                    //     web::resource("/register/{invitation_id}")
+                    //         .route(web::post().to(register_handler::register_user)),
+                    // )
+                    // .service(
+                    //     web::resource("/auth")
+                    //         .route(web::post().to(auth_handler::login))
+                    //         .route(web::delete().to(auth_handler::logout))
+                    //         .route(web::get().to(auth_handler::get_me)),
+                    // ),
+            )
+    })
+    .bind("127.0.0.1:3000")?
+    .run()
+    .await
 }
